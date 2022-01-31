e65fdaff1719bd1565bb12f35b85a00e21bae941
kento-furusho <84021293+kento-furusho@users.noreply.github.com>
2022-01-31 11:37:18 +0900

    add axiosjs

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <Tutorial />
+  <div>
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+    >
+      RailsからAPIを取得する
+    </button>
+    <div
+      v-for="(msg, i) in msgs"
+      :key="i"
+    >
+      {{ msg }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  data () {
+    return {
+      msgs: []
+    }
+  },
+  methods: {
+    getMsg () {
+      this.$axios.$get('/api/v1/hello')
+        // res=response, push:配列のお尻に追加
+        .then(res => this.msgs.push(res))
+    }
+  }
 }
 </script>
