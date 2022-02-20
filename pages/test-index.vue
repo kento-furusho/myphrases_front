96e77a80ba93573e65141001c619b85853e19052
kento-furusho <84021293+kento-furusho@users.noreply.github.com>
2022-02-20 17:09:01 +0900

    introduce devise_token_auth & getTable from api

---
+++ pages/test-index.vue
@@ -0,0 +1,120 @@
+<template>
+  <div id="app">
+    <h3>テスト</h3>
+    <div v-if="client === ''">
+      <div>
+        <h1>SignUp</h1>
+        <label for="name">name</label>
+        <input id="name" v-model="name" type="name">
+        <label for="email">email</label>
+        <input id="email" v-model="email" type="email">
+        <label for="password">password</label>
+        <input id="password" v-model="password" type="password">
+        <button @click="signup">
+          新規登録
+        </button>
+      </div>
+      <div>
+        <h1>SignIn</h1>
+        <label for="name">name</label>
+        <input id="name" v-model="name" type="name">
+        <label for="email">email</label>
+        <input id="email" v-model="email" type="email">
+        <label for="password">password</label>
+        <input id="password" v-model="password" type="password">
+        <button @click="signin">
+          SignIn
+        </button>
+      </div>
+    </div>
+    <div v-else>
+      <button @click="signout">
+        ログアウト
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      uid: '',
+      access_token: '',
+      client: '',
+      title: '',
+      content: '',
+      tasks: [],
+      comment: '',
+      posts: []
+    }
+  },
+  methods: {
+    signup () {
+      axios
+        .post('http://localhost:3001/api/v1/auth', {
+          name: this.name,
+          email: this.email,
+          password: this.password
+        })
+        .then((response) => {
+          localStorage.setItem('access-token', response.headers['access-token'])
+          localStorage.setItem('client', response.headers.client)
+          localStorage.setItem('uid', response.headers.uid)
+          this.access_token = response.headers['access-token']
+          this.client = response.headers.client
+          this.uid = response.headers.uid
+        })
+    },
+    signin () {
+      axios
+        .post('http://localhost:3001/api/v1/auth/sign_in', {
+          name: this.name,
+          email: this.email,
+          password: this.password
+        })
+        .then((response) => {
+          localStorage.setItem('access-token', response.headers['access-token'])
+          localStorage.setItem('client', response.headers.client)
+          localStorage.setItem('uid', response.headers.uid)
+          this.access_token = response.headers['access-token']
+          this.client = response.headers.client
+          this.uid = response.headers.uid
+        })
+    },
+    signout () {
+      axios
+        .delete('http://localhost:3001/api/v1/auth/sign_out', {
+          test: { test: 'test' },
+          headers: {
+            uid: this.uid,
+            'access-token': this.access_token,
+            client: this.client
+          }
+        })
+        .then((response) => {
+          this.access_token = ''
+          this.client = ''
+          this.uid = ''
+          localStorage.removeItem('uid')
+          localStorage.removeItem('access-token')
+          localStorage.removeItem('client')
+        })
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
