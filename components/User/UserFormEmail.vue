e1ad583853e7b06de8383f0a58ce39312c0af5ba
kento-furusho <84021293+kento-furusho@users.noreply.github.com>
2022-03-05 16:27:44 +0900

    set data vinding for signup.vue

--- components/User/UserFormEmail.vue
+++ components/User/UserFormEmail.vue
@@ -1,5 +1,6 @@
 <template>
   <v-text-field
+    v-model="setEmail"
     label="メールアドレスを入力"
     placeholder="your@email.com"
     outlined
@@ -7,5 +8,21 @@
 </template>
 <script>
 export default {
+  props: {
+    email: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    setEmail: {
+      get () {
+        return this.email
+      },
+      set (newVal) {
+        return this.$emit('update:email', newVal)
+      }
+    }
+  }
 }
 </script>
--- components/User/UserFormPassword.vue
+++ components/User/UserFormPassword.vue
@@ -1,5 +1,6 @@
 <template>
   <v-text-field
+    v-model="setPassword"
     label="パスワードを入力"
     placeholder="8文字以上"
     outlined
@@ -7,5 +8,21 @@
 </template>
 <script>
 export default {
+  props: {
+    password: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    setPassword: {
+      get () {
+        return this.password
+      },
+      set (newVal) {
+        return this.$emit('update:password', newVal)
+      }
+    }
+  }
 }
 </script>
